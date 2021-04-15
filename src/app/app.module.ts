83be7cf1c3edb11add1264bb3a544ec49dfa408f
Cristofer david <cristoferdaviduv@gmail.com>
2021-04-15 19:00:24 +0200

    añadido el componente del producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HeaderComponent
+      HeaderComponent,
+      ProductComponent
    ],
   imports: [
     BrowserModule,
