1b191191e0182dc599cba3d3ca95c8e850aeeba1
Freyja96 <cristina.oue@gmail.com>
2021-04-17 11:50:44 +0200

    añadido nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductComponent } from './product/product.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [			
     AppComponent,
       HeaderComponent,
-      ProductComponent
+      ProductComponent,
+      EditProfileComponent
    ],
   imports: [
     BrowserModule,
