0c4b1f9ba9ab3c5e33e78ae2d154834ef6e262d9
Freyja96 <cristina.oue@gmail.com>
2021-04-18 14:36:26 +0200

    Estructura de editar perfil y error 404

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,11 @@ export class HeaderComponent implements OnInit {
 
   titulo = 'MyGardenToYours';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+navigateToSearch(){
+  this.router.navigate(['/search'])
+}
 }
