5b4fc75e050957b3414c47cdd3e784a32bc3dd0f
dm94 <dm94@hotmail.es>
2021-04-21 18:55:21 +0200

    Mejora de el perfil publico

--- src/app/public-profile/public-profile.component.ts
+++ src/app/public-profile/public-profile.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-public-profile',
   templateUrl: './public-profile.component.html',
-  styleUrls: ['./public-profile.component.scss']
+  styleUrls: ['./public-profile.component.scss'],
 })
 export class PublicProfileComponent implements OnInit {
+  products = [1, 2, 3, 5, 6, 7, 8];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
