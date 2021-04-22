17dd9a4c40d5f9d1c64b8194ba86fbdfa6355a2b
Freyja96 <cristina.oue@gmail.com>
2021-04-22 20:21:13 +0200

    hasta aquí por hoy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
+import { ProductComponent } from './../product/product.component';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  titulo="MyGardenToYours";
   constructor() { }
 
   ngOnInit() {
