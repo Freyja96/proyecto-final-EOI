b159865b2015fd85e75891fc531278d6da4ecd61
Freyja96 <cristina.oue@gmail.com>
2021-05-14 20:23:07 +0200

    Metidos un par de cambios (still working yet)

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,16 +1,7 @@
 export class User{
-  _id?: string;
   firstName?: string;
   email?: string;
-  username?: string;
   password?: string;
   lastName?: string;
   dateOfBirth?: string;
-  emailVerified?: boolean;
-  createDate?: string;
-  image?: {
-    _id?: string;
-    url?: string;
-  };
-  location?: string;
 }
