5503946c1903afb16a087bf42a1aa48f5713c92d
dm94 <dm94@hotmail.es>
2021-05-15 17:13:34 +0200

    Creado user service

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,16 +1,16 @@
-/* tslint:disable:no-unused-variable */
+import { TestBed } from '@angular/core/testing';
 
-import { TestBed, async, inject } from '@angular/core/testing';
 import { UserService } from './user.service';
 
-describe('Service: User', () => {
+describe('UserService', () => {
+  let service: UserService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [UserService]
-    });
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(UserService);
   });
 
-  it('should ...', inject([UserService], (service: UserService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private httpClient: HttpClient) {}
+
+  getUser(): Observable<any> {
+    return this.httpClient.get(`${environment.apiUrl}/users`).pipe(
+      catchError((error) => {
+        return error;
+      })
+    );
+  }
+
+  updateUser(data: Object, action: string): Observable<any> {
+    return this.httpClient
+      .patch(`${environment.apiUrl}/users`, {
+        params: { action: action },
+        body: data,
+      })
+      .pipe(
+        catchError((error) => {
+          return error;
+        })
+      );
+  }
+
+  deleteUser(): Observable<any> {
+    return this.httpClient.delete(`${environment.apiUrl}/users`).pipe(
+      catchError((error) => {
+        return error;
+      })
+    );
+  }
+}
