bf72d1b9d8104a1b1e71911b923b7d9349cc4a4c
dm94 <dm94@hotmail.es>
2021-05-18 11:54:39 +0200

    Añadido los http

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -7,67 +7,77 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
   titulo = 'GardenForYou';
   loginForm: FormGroup;
   logged = false;
-  error = "";
+  error = '';
 
   constructor(
     private router: Router,
     private userService: UserService,
     private formBuilder: FormBuilder,
-    private authService: AuthService)
-    {
+    private authService: AuthService
+  ) {
     this.loginForm = this.formBuilder.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required]
-    })
-   };
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
-  get f(){
-    return this.loginForm.controls
-  };
+  get f() {
+    return this.loginForm.controls;
+  }
 
-  login(){
+  login() {
     let email = this.f.email.value;
     let password = this.f.password.value;
 
-    const user: User = new User()
+    const user: User = new User();
 
     user.email = email;
     user.password = password;
 
-    this.userService.userLogin(user).subscribe((data: any) => {
-      localStorage.setItem("token",data.access_token)
-      this.router.navigate(["/"])
-    },
-      error => {
-        console.log("Error:", error);
-        this.error="El email o contraseña no son correctos.";
+    this.userService.userLogin(user).subscribe(
+      (data: any) => {
+        localStorage.setItem('token', data.access_token);
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        if (error.status == 500) {
+          this.error = 'No se ha podido conectar con el servidor';
+        } else {
+          this.error = 'El email o contraseña no son correctos.';
+        }
       }
     );
- };
+  }
 
-  redirectLogin(){
-    this.userService.getUser().subscribe((data: any) => {
-      if(data.emailVerified){
-        this.router.navigate(["/"])
-      } else {
-        this.router.navigate(["/validate"])
-      }
-    },
-      error => {
-        console.log("Error:", error);
+  redirectLogin() {
+    this.userService.getUser().subscribe(
+      (data: any) => {
+        if (data.emailVerified) {
+          this.router.navigate(['/']);
+        } else {
+          this.router.navigate(['/validate']);
+        }
+      },
+      (error) => {
+        if (error.status == 500) {
+          this.error = 'No se ha podido conectar con el servidor';
+        } else if (error.status == 401) {
+          localStorage.clear();
+          this.router.navigate(['/']);
+        }
       }
     );
-  };
+  }
 
   ngOnInit() {
-    if(this.logged = this.authService.isAuthenticated()){
-      this.router.navigate(['/'])
+    if ((this.logged = this.authService.isAuthenticated())) {
+      this.router.navigate(['/']);
     }
   }
 }
