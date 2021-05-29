4567ed38cea2fb02b494749ceddb5e4109696a18
Freyja96 <cristina.oue@gmail.com>
2021-05-29 18:55:59 +0200

    solucionado problema en p.públ. y dropdown en header.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  titulo = 'GardenForYou';
   logged = false;
   products = [];
 
